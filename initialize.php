ee517c60a058aa5d91b2fd09947ddef3239f7f1e
Test <dothanhdatkhmt@gmail.com>
2024-11-22 03:24:50 +0700

    upload source code

--- initialize.php
+++ initialize.php
@@ -6,5 +6,5 @@ if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"12345678");
-if(!defined('DB_NAME')) define('DB_NAME',"ocwbs_db");ocw
+if(!defined('DB_NAME')) define('DB_NAME',"ocwbs_db");
 ?>
